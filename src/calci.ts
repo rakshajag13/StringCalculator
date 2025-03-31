bdcacec538ca9ea8108a5bd79ea9e969532fd867
Raksha Pawar <raksha.jag13@gmail.com>
2025-03-31 13:56:53 +0530

    add support for custom delimiters in string calculator

--- src/calci.ts
+++ src/calci.ts
@@ -2,18 +2,23 @@ function add(numbers: string): number {
   if (!numbers.length) {
     return 0;
   }
-  if (numbers.length === 1) {
-    return parseInt(numbers);
-  }
-  if (numbers.length > 1) {
-    const numArray = numbers.split(/[\n,]/).map((num) => parseInt(num));
-    if (numArray.some((num) => isNaN(num))) {
-      return 0;
-    }
-    return numArray.reduce((acc, curr) => acc + curr, 0);
+
+  let delimiter = /[\n,]/;
+  let numString = numbers;
+
+  // Check for custom delimiter
+  if (numbers.startsWith("//")) {
+    const delimiterEndIndex = numbers.indexOf("\n");
+    delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
+    numString = numbers.substring(delimiterEndIndex + 1);
   }
 
-  return 0;
+  const numArray = numString.split(delimiter).map((num) => parseInt(num));
+  if (numArray.some((num) => isNaN(num))) {
+    return 0;
+  }
+
+  return numArray.reduce((acc, curr) => acc + curr, 0);
 }
 
 export default add;
--- tests/calci.test.ts
+++ tests/calci.test.ts
@@ -22,7 +22,12 @@ describe("String Calculator", () => {
     expect(add("1\n2,3")).toBe(6);
     expect(add("1\n2\n3")).toBe(6);
   });
+
   test("should fail for invalid input string", () => {
     expect(add("1,\n")).toBe(0);
+  });
+
+  test("should handle custom delimiters", () => {
+    expect(add("//;\n1;2")).toBe(3);
   });
 });
